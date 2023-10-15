704eaf9e20f8a579d6e9e94f5c08bf6739ef2db7
Caleb Rivera <calebrossr@gmail.com>
2023-10-15 07:24:53 -0400

    add prisma

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,33 @@
 import { createServer } from "http";
 import { createSchema, createYoga } from "graphql-yoga";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const yoga = createYoga({
   schema: createSchema({
     typeDefs: /* GraphQL */ `
+      type User {
+        id: ID!
+        name: String!
+        email: String!
+      }
+
       type Query {
         hello: String
+        getUser(id: ID!): User
       }
     `,
     resolvers: {
       Query: {
         hello: () => "Hello from Yoga!",
+        getUser: (_, { id }) => {
+          return prisma.user.findUnique({
+            where: {
+              id: Number(id),
+            },
+          });
+        },
       },
     },
   }),
